f530bb2f2681e3113d3b914610128c8373d9d41b
Pochobs <pochobs@hotmail.com>
2020-06-21 23:29:52 -0500

    Html

---
+++ script.js
@@ -0,0 +1,25 @@
+
+var m = moment();
+console.log(moment());
+
+
+
+
+
+
+
+
+
+
+
+
+
+// $("#09 .description").val(localStorage.getItem("09"));
+// $("#10 .description").val(localStorage.getItem("10"));
+// $("#11 .description").val(localStorage.getItem("11"));
+// $("#12 .description").val(localStorage.getItem("12"));
+// $("#13 .description").val(localStorage.getItem("13"));
+// $("#14 .description").val(localStorage.getItem("14"));
+// $("#15 .description").val(localStorage.getItem("15"));
+// $("#16 .description").val(localStorage.getItem("16"));
+// $("#17 .description").val(localStorage.getItem("17"));
